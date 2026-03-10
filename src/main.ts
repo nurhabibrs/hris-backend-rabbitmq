daa04abf306f6ca2897b83ffb77dc69adac4d622
nurhabibrs <nurhabibrizkisaputro@gmail.com>
2026-03-11 04:42:19 +0700

    fix: remove unused RABBITMQ_URL environment variable and revert to default RabbitMQ URL

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,10 @@ import { AppModule } from './app.module';
 import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const rabbitmqUrl = process.env.RABBITMQ_URL ?? 'amqp://localhost:5672';
-
   const app = await NestFactory.createMicroservice(AppModule, {
     transport: Transport.RMQ,
     options: {
-      urls: [rabbitmqUrl],
+      urls: ['amqp://localhost:5672'],
       queue: 'logs_queue',
       queueOptions: {
         durable: true,
